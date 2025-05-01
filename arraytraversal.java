d17a481f2b4bd6b4f8a5beaeb76eff594803f305
saiharshitha055 <bujjiluharshikolluri@gmail.com>
2025-05-01 21:24:33 +0530

    Add files via upload

---
+++ arraytraversal.java
@@ -0,0 +1,19 @@
+//Traversal array
+public class arraytraversal{
+    Run|Debug
+public static void main(String[]args)    {
+int[] numbers=new int[5];
+String[] names={"Amit","Anusuya","Rinku","Aloke","Purnima"};
+System.out.println(X:"Number Array:");
+for(int i=0;i<numbers.length;i++){
+    System.out.println(numbers[i]);
+}
+System.out.printin(x:"Names array:");
+for(int i=0;i<names.length;i++){
+    System.out.println(names[i]);
+}
+}
+}
+
+
+
